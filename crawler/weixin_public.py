d2e48242871f4bfcbdc5ebfa2087f0e38fcbd995
Spirit <spirit.dongdong@gmail.com>
2016-07-13 15:28:12 +0800

    using cookie & firefox to against anti-spider

--- crawler/weixin_public.py
+++ crawler/weixin_public.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import re
 import datetime
 import json
+from pyvirtualdisplay import Display
 
 from DBUtils.PooledDB import PooledDB
 import MySQLdb
@@ -27,11 +28,11 @@ head_tag = """
 """
 
 # phantomjs_path = '/server/phantomjs-2.1.1/bin/phantomjs'
-phantomjs_path = '/usr/local/bin/phantomjs'
-dcap = dict(DesiredCapabilities.PHANTOMJS)
-dcap["phantomjs.page.settings.userAgent"] = (
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-)
+# phantomjs_path = '/usr/local/bin/phantomjs'
+# dcap = dict(DesiredCapabilities.PHANTOMJS)
+# dcap["phantomjs.page.settings.userAgent"] = (
+#     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+# )
 base_url = 'http://mp.weixin.qq.com'
 # base_dir = '/Users/Spirit/Downloads/weixin_public/'
 base_dir = '/home/Spirit/weixin_public/'
@@ -44,8 +45,8 @@ public_name_path = '/home/Spirit/python-crawler/crawler/weixin.txt'
 # pool = PooledDB(MySQLdb, 3, host='192.168.2.96', user='root',
 #                 passwd='akQq5csSXI5Fsmbx5U4c', db='zhisland_base', port=3306, charset='utf8')
 
-pool = PooledDB(MySQLdb, 3, host='192.168.2.68', user='zhisland_bms',
-                passwd='zhisland', db='zh_bms_cms', port=3306, charset='utf8')
+pool = PooledDB(MySQLdb, 3, host='192.168.2.96', user='root',
+                passwd='akQq5csSXI5Fsmbx5U4c', db='zh_bms_cms', port=3306, charset='utf8')
 
 headers = {
     'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -65,34 +66,39 @@ def has_crawled(public_name, title, cur):
     return True
 
 def crawl():
+
+    # for linux headless brower
+    display = Display(visible=0, size=(800, 600))
+    display.start()
+
     conn = pool.connection()
     cur = conn.cursor()
-    count = 0
+
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("general.useragent.override","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:46.0) Gecko/20100101 Firefox/46.0")
+    fp.update_preferences()
+
+    # count = 0
     for public_name in open(public_name_path):
+
+
         public_name = public_name.strip().encode('utf-8')
-        # path = base_dir + str(count)
-        count = count + 1
-        # if not os.path.exists(path):
-        #     os.mkdir(path)
+
         #根据公众号名称搜索, 得到列表
         url = 'http://weixin.sogou.com/weixin?type=1&query=%s&ie=utf8&_sug_=n&_sug_type_=' % quote(public_name)
-        r = requests.get(url, headers = headers)
-        soup = BeautifulSoup(r.text, 'html.parser')
-
-        public_temp_link = soup.find('div', {'target':'_blank', 'href':True})
-
-        if public_temp_link is None:
-            print(soup)
-            print("%s public link is None" % public_name)
+        driver = webdriver.Firefox(firefox_profile=fp)
+        driver.get(url)
+        time.sleep(5)
+        elem = driver.find_element_by_class_name('txt-box')
+        elem.click()
+        time.sleep(8)
+        hs = driver.window_handles
+        if len(hs) != 2:
+            print 'window_handles length: %d, error!' % len(hs)
             continue
 
+        driver.switch_to.window(hs[1])
 
-        public_link = public_temp_link.get('href')
-
-        time.sleep(random.uniform(5, 8))
-        driver = webdriver.PhantomJS(phantomjs_path, desired_capabilities=dcap)
-        driver.get(public_link)
-        time.sleep(random.uniform(5, 8))
         soup2 = BeautifulSoup(driver.page_source, 'html.parser')
 
         #得到近期的文章列表
@@ -106,6 +112,7 @@ def crawl():
             print('href, title, time not equal, exit')
             continue
 
+        print("%s has %s articles" % (public_name, len(hrefs)))
         for idx, href in enumerate(hrefs):
             title = href.get_text().strip().encode('utf-8')
 
@@ -152,19 +159,6 @@ def crawl():
             else:
                 author = 'unKnown'
 
-            #format
-            #去掉"微信扫一扫"
-            # if artical_soup.find('div', class_='qr_code_pc') is not None:
-            #     [x.extract() for x in artical_soup.find_all('div', class_='qr_code_pc')]
-            # #去掉"相关文章"
-            # if artical_soup.find('div', {'id':'sg_tj'}) is not None:
-            #     [x.extract() for x in artical_soup.find_all('div', {'id':'sg_tj'})]
-            # #去掉"精选留言"
-            # if artical_soup.find('div', class_='rich_media_area_extra') is not None:
-            #     [x.extract() for x in artical_soup.find_all('div', class_='rich_media_area_extra')]
-            # if artical_soup.find('div', class_='rich_media_tool') is not None:
-            #     [x.extract() for x in artical_soup.find_all('div', class_='rich_media_tool')]
-
             #对图片的修改
             if artical_soup.find_all('img', {'data-src':True, 'src':True}) is not None:
                 for e in artical_soup.find_all('img', {'data-src':True, 'src':True}):
@@ -180,101 +174,6 @@ def crawl():
                     except Exception as e:
                         print(e)
                         continue
-
-            #底部的qq音乐之类
-            # if artical_soup.find('script', {'id':'qqmusic_tpl'}) is not None:
-            #     artical_soup.find('script', {'id':'qqmusic_tpl'}).extract()
-            # if artical_soup.find('script', {'id':'voice_tpl'}) is not None:
-            #     artical_soup.find('script', {'id':'voice_tpl'}).extract()
-            #
-            # #转载相关
-            # copyright_info = artical_soup.find('a', {'id':'copyright_info'})
-            # if copyright_info is not None:
-            #     copyright_info.extract()
-
-
-            #针对一些特定微信号的处理
-            # if count == 3: #占豪
-            #     removes = []
-            #     a = artical_soup.find('section', {'style':'white-space: normal; font-family: 微软雅黑; line-height: 28.4444px; box-sizing: border-box; border: 0px none;'})
-            #     if a is not None and a.next_elements is not None:
-            #         for es in a.next_elements:
-            #             if type(es) == bs4.element.Tag:
-            #                 removes.append(es)
-            #         for es in removes:
-            #             es.extract()
-            #         a.extract()
-                # a = artical_soup.find('span', text='淘宝特约店址：http://goldengame.taobao.com [长按复制]')
-                # a.extract()
-            # elif count == 4: #深蓝财经网
-            #     removes = []
-            #     a = artical_soup.find('span', text='深蓝财经网 微信号：shenlancaijing').parent
-            #     if a is not None:
-            #         for es in a.next_elements:
-            #             if type(es) == bs4.element.Tag:
-            #                 removes.append(es)
-            #         for es in removes:
-            #             es.extract()
-            #         a.extract()
-            # elif count == 5: #新财富杂志
-            #     gifs = artical_soup.find_all('img', {'data-type':'gif'})
-            #     if len(gifs) > 2:
-            #         total_gifs = len(gifs)
-            #         gifs[0].extract()
-            #         gifs[total_gifs-1].extract()
-            #
-            # elif count == 6: #华商韬略
-            #     a = soup.find('div',{'id':'js_content'}).find_all('section', recursive=False)
-            #     if a is not None:
-            #         last_span = a[-1].find_all('span', recursive=False)[-1]
-            #         if last_span is not None:
-            #             last_p = last_span.find_all('p', recursive=False)[-1]
-            #             last_p.extract()
-            # elif count == 9: #环球老虎财经
-            #     ss = artical_soup.find('div',{'id':'js_content'}).find_all('section', recursive=False)
-            #     if ss is not None:
-            #         ss[-1].extract()
-            # elif count == 8: #财经杂志, 暂缓
-            #     last_p = soup.find('div',{'id':'js_content'}).find_all('p', recursive=False, attrs={'style':True})
-            #     if last_p is not None:
-            #         last_p[-1].extract()
-            # elif count == 7: #扑克投资家  感觉可能有版权, 先不搞了
-            #     a = artical_soup.find('span', text='微信更新好，记得置顶哦')
-            #     if a is not None:
-            #         a.parent.parent.extract()
-            #
-            #     b = soup.find('span', text='获取更多资讯')
-            #     if b is not None:
-            #         removes = []
-            #         for es in b.parent.parent.next_elements:
-            #             if type(es) == bs4.element.Tag:
-            #                 removes.append(es)
-            #             for es in removes:
-            #                 es.extract()
-            #         b.parent.parent.extract()
-            # elif count == 10: #新经济100人
-            #     ss = artical_soup.find('div', {'id':'img-content'}).find_all('section', {'data-brushtype':'text'})
-            #     if len(ss) > 0:
-            #         removes = []
-            #         for es in ss[-1].parent.next_elements:
-            #             if type(es) == bs4.element.Tag:
-            #                 removes.append(es)
-            #             for es in removes:
-            #                 es.extract()
-            # elif count == 15: #企业头条
-            #     ss = artical_soup.find('div', {'id':'js_content'}).find_all('section', recursive=False, _class=None)
-            #     if ss is not None:
-            #         ss[-1].extract()
-            # elif count == 16: #创业家
-            #     a = artical_soup.find('p', text='本文为创业家原创，受访者及123RF供图。转载请后台回复“转载”，未经授权，转载必究。')
-            #     if a is not None:
-            #         removes = []
-            #         for es in a.next_elements:
-            #             if type(es) == bs4.element.Tag:
-            #                 removes.append(es)
-            #             for es in removes:
-            #                 es.extract()
-
 
             artical_copy_soup = BeautifulSoup(str(artical_soup), 'html.parser')
             src_read = tiny(artical_copy_soup)
@@ -297,12 +196,11 @@ def crawl():
                 print(e)
                 print("%s, %s crawl error" % (public_name, title))
                 continue
-            # hash = abs(title.__hash__())
-            # output_file = open(path + '/' + str(hash) + '.html', mode='w')
-            # output_file.write(str(artical_soup))
-            # output_file.close()
+
             print("%s done" % (title))
+            time.sleep(3)
         print("%s has done" % public_name)
+        time.sleep(20)
 
 
         driver.delete_all_cookies()
